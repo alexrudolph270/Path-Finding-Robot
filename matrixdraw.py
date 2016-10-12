6917ad4bef86b0319d91213942220a9f3b889b51
Peter1013 <pedro.delosreyes487@myci.csuci.edu>
2016-10-12 16:47:32 -0700

    Add files via upload

--- matrixdraw.py
+++ matrixdraw.py
@@ -1,10 +1,8 @@
 # Path Mode GUI dealy
-# currently breaks if you try to backtrack out of order
-# works otherwise
 # prints alist of path elements(coordinates) so far, final version will send 
 #	list to the direction module
 
-#  to change grid "resolution" change x and y ranges in instantiation
+#  to change grid "resolution" dimensions function
 
 
 from tkinter import *
@@ -17,7 +15,6 @@ pathButtons = []
 # keeps 2d list of coordinates representing the path to take
 path = []
 # used for keeping track of selection/deselection
-order = [] #might not be necessary
 # keeps track of number of path buttons selected to maintain order
 orderindex = 0
 
@@ -32,14 +29,12 @@ def clickpath(x, y):
 	but = pathButtons[x][y] #acces button from grid buttons
 	if(but.cget('text') == orderindex): # if button has text tag
 		but.configure(text=" ") #set it blank
-		order.pop() #remove last order[] element (order of coordinates)
 		path.pop() #remove last patth[] element (coordinate)
 		orderindex -= 1
-	else:
+	elif(but.cget('text') == ' '):
 		but.configure(text=orderindex+1) #add order index to button text tag
-		order.append(orderindex+1) #append order index to order[]
-		path.append([x, y]) #append coordinates to path[]
-		orderindex += 1 
+		path.append([x, y]) #append 
+		orderindex += 1
 
 def clickstart():
 	# executes start button function(s)
@@ -47,7 +42,6 @@ def clickstart():
 	# will eventually send list of coordinates to the 'directions' module
 	print(path)
 	print(orderindex)
-	print(order)
 
 # Instantiation of grid and start buttons
 for x in range(dimensions()):
