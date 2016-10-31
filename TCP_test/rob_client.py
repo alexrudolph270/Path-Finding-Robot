6c9202e84e855b6f654915293bac6699ebe4e89c
stormon-force <stormon.force211@myci.csuci.edu>
2016-10-31 12:16:50 -0700

    Update rob_client.py

--- TCP_test/rob_client.py
+++ TCP_test/rob_client.py
@@ -7,15 +7,20 @@ port = 8002
 
 s.connect((host,port))
 
-data = s.recv(64)
-print(data)
+data = s.recv(64) #when connected will receive up to 64 bytes
+print(data)       #print received data, probably the IP address of server
+s.send("Client is connected".encode())
 
-s.send("sentFromClient".encode())
-
+#'''
 outbound = "start" 
 
 while outbound != "quit":
   outbound = input("Enter command: ")
-  print("sending outbound: ",outbound)
-  s.send(outbound.encode())
+  send_command(outbound)
+#'''
+  
+  #simple function that will be used for every time you call a gopigo function
+def send_command(str)
+  print("Sending string: ",str)
+  s.send(str.encode())
   
