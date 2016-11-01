1b31a454ba412a17859c425b9e465f775e80a8af
Alindah <AlindaHeng@gmail.com>
2016-11-01 14:04:02 -0700

    Update linux_dc.py

--- linux_dc.py
+++ linux_dc.py
@@ -10,48 +10,37 @@ import os
 
 # from gopigo import *
 
-
 os.system("xset r off")
 
-done = 1  # Check if robot is done moving.
-first_key = None  # Initial key pressed.
-current_key = None  # Used to check if current key is same as initial.
+done = 1
 
-'''
-    check() will check if the user is holding down a key. Currently, we are
-    only testing keys 'w' and 's'.
-    '''
+def end(event):
+    global done
+    done = 1
+    print("Stop")
+    #stop()
 
 def key_input(event):
     global done
-    global first_key
     key_press = event.keysym.lower()
 
     print(key_press)
-
-    if key_press == 'w':
-        first_key = key_press
-        if done == 1:
+    
+    while done != 1:
+        if key_press == 'w':
             done = 0
             print("Robot is moving forward.")
             #fwd()
 
-    elif key_press == 's':
-        first_key = key_press
-        if done == 1:
+        elif key_press == 's':
             done = 0
             print("Robot is moving backwards.")
-            # bwd()
-
-
-def end(event):
-    print("Stop")
-    # stop()
-
+            #bwd()
+        
+        command.bind_all("<KeyRelease>", end)
 
 command = tk.Tk()
 command.bind_all("<KeyPress>", key_input)
-command.bind_all("<KeyRelease>", end)
 command.mainloop()
 
 os.system("xset r on")
