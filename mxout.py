2a3ba4d2c78b3d5081eb081d36142c4102861b47
Peter1013 <pedro.delosreyes487@myci.csuci.edu>
2016-11-10 14:49:11 -0800

    Update mxout.py

--- mxout.py
+++ mxout.py
@@ -1,5 +1,9 @@
 ###############################################################################
 ## Map Mode gui output testing
+##  latest version just cleans up unnecessary code and adds relevant comments
+##  this version will be running on the client side, and will await a matrix from
+##  map mode on the robot to display. Not sure exactly how that's supposed to
+##  happen. This version will be mashed into main as its own class very soon!
 #####################################################################t#########
 from tkinter import *
 ###############################################################################
@@ -18,19 +22,22 @@ m = [[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
 
 weem = []
 
+#dimensions of window are dependant of dimensions of canvas
+#  canvas is NOT dynamic at the moment, matrices with very different
+#  length and width will appear stretched depending on the difference
 canvasX = 900
 canvasY = 900
 
-min_y = 9999 # arbitrarily high
-max_y = 0 #arbitrarily low, no negs.
-
+# removes rows with no relevant data
 for x in m:
     if('x' in x):
         weem.append(x)
 
-#for x in weem:
-#    print(x)
+min_y = 9999 # arbitrarily high
+max_y = 0 #arbitrarily low, no negs.
 
+#slices remaining rows based on leftmost(min_y) relevant data and
+#  rightmost(max_y) relevant data
 for x in weem:
     for y in range (len(x)):
         if(x[y] == 'x'):
@@ -39,37 +46,23 @@ for x in weem:
             if(max_y < y):
                 max_y = y
 
-print(min_y, max_y)
 for x in range(len(weem)):
     weem[x] = weem[x][min_y:max_y+1]
 
-for x in weem:
-    print(x)
+#determines dimensions of individual squares, the "resolution" of the mapping
+xlen = int(canvasX/len(weem[0]))
+ylen = int(canvasY/len(weem))
 
-xlen = int(900/len(weem[0]))
-ylen = int(900/len(weem))
-
-print(xlen, ylen)
-
+#windos/tk stuff
 root = Tk()
-
-can = Canvas(root, width=900, height=900)
+can = Canvas(root, width=canvasX, height=canvasY)
 can.pack()
-
-counter = 1
+#draws the squares on screen
 for y in range(len(weem)):
     for x in range(len(weem[0])):
         if (weem[y][x] == ' '):
             can.create_rectangle(x*xlen, y*ylen, (x+1)*xlen, (y+1)*ylen)
-            print(x, y)
         else:
             can.create_rectangle(x*xlen, y*ylen, (x+1)*xlen, (y+1)*ylen, fill="black")
-            print(x, y)
-        counter += 1
-
-#can.create_rectangle(0,0,300,300, fill="red")
-#can.create_rectangle(300,0,600,300, fill="black")
-#can.create_rectangle(0,300,300,600, fill="black")
-#can.create_rectangle(300,300,600,600, fill="red")
 
 mainloop()
