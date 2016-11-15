5f0ff1072fbb6d7859ab2e27d31ecf064f30101a
baileydurkin <bailey.durkin001@myci.csuci.edu>
2016-11-14 16:05:23 -0800

    Update map_mode.py
    - refined console output to be more readable
    - added code that makes the robot back up if it is too close to the wall

--- MAP/map_mode.py
+++ MAP/map_mode.py
@@ -6,47 +6,147 @@
 from gopigo import *
 import time
 
-## Initialize the matrix
-i, j = 100, 100
-map_mtx = [['-' for x in range(i)] for y in range(j)]
-
-## Define constants
-WALL = 50       # Minimum distance (in cm) for wall
-MOVE_DIST = 36  # Number of clicks to move (1 click ~= 1.13 cm) ## WALL > MOVE_DIST * 1.13 ##
-UP = 0
-RIGHT = 1
-DOWN = 2
-LEFT = 3
-MOVE_FORWARD = 0
-MOVE_RIGHT = 1
-MOVE_LEFT = 2
-
-## Set start parameters
-current_x = 50
-current_y = 50          # robot starts in the middle of the matrix
-current_d = UP          # robot starts facing UP; top of matrix is relative to robots starting orientation
-start_x = -1
-start_y = -1
-
+## map_mode() uses the robot and two ultrasonic sensors to navigate and create an
+## apporximate map of the perimerter of a room. Coordinates are represented as 
+## coordinate pairs inside a matrix. Returns the map matrix after mapping is complete.
 def map_mode():
-    # import globals that need to be modified  (read-only variables don't need to be imported)
+    # create globals
     global current_x
     global current_y
     global current_d
     global start_x
     global start_y
+    global map_mtx
+
+    # Initialize the matrix
+    i, j = 100, 100
+    map_mtx = [['-' for x in range(i)] for y in range(j)]
+
+    # Define constants
+    WALL = 25       # Minimum distance (in cm) for wall
+    MOVE_DIST = 20  # Number of clicks to move (1 click ~= 1.13 cm) ## WALL > MOVE_DIST * 1.13 ##
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+    MOVE_FORWARD = 0
+    MOVE_RIGHT = 1
+    MOVE_LEFT = 2
+
+    # Set start parameters
+    current_x = 50
+    current_y = 50          # robot starts in the middle of the matrix
+    current_d = UP          # robot starts facing UP; top of matrix is relative to robots starting orientation
+    start_x = -1
+    start_y = -1
+
+    ## addWall() adds the wall to the left of the robot to the map matrix 
+    ## relative to the robot's current position.
+    def addWall():
+        # import globals that need to be modified
+        global map_mtx
+
+        # add wall relative to current orientation of the robot
+        if (current_d == UP):
+            map_mtx[current_x - 1][current_y] = 'X'
+            print("[WALL] ", current_x - 1, ",", current_y, " added to matrix")
+        elif (current_d == RIGHT):
+            map_mtx[current_x][current_y - 1] = 'X'
+            print("[WALL] ", current_x, ",", current_y - 1, " added to matrix")
+        elif (current_d == DOWN):
+            map_mtx[current_x + 1][current_y] = 'X'
+            print("[WALL] ", current_x + 1, ",", current_y, " added to matrix")
+        elif (current_d == LEFT):
+            map_mtx[current_x][current_y + 1] = 'X'
+            print("[WALL] ", current_x, ",", current_y + 1, " added to matrix")
+        else:
+            print("[ERROR] invalid direction")
+            return -1
+
+    ## updatePos() updates the position of the robot when given a movement direction
+    ## both the current position and the current direction are updated
+    def updatePos(move_direction):
+        # import globals that need to be modified
+        global current_x
+        global current_y
+        global current_d
+
+        if (move_direction == MOVE_FORWARD):
+            if (current_d == UP):
+                current_y = current_y - 1
+            elif (current_d == RIGHT):
+                current_x = current_x + 1
+            elif (current_d == DOWN):
+                current_y = current_y + 1
+            elif (current_d == LEFT):
+                current_x = current_x - 1
+            else:
+                print("[ERROR] invalid direction")
+                return -1
+
+            print("[DIRECTION] current direction is now ", current_d)
+            print("[POSITION] Current position is now ", current_x, ",", current_y)
+
+        elif (move_direction == MOVE_RIGHT):
+            if (current_d == UP):
+                current_d = RIGHT
+                current_x = current_x + 1
+            elif (current_d == RIGHT):
+                current_d = DOWN
+                current_x = current_y + 1
+            elif (current_d == DOWN):
+                current_d = LEFT
+                current_y = current_x - 1
+            elif (current_d == LEFT):
+                current_d = UP
+                current_x = current_y - 1
+            else:
+                print("[ERROR] invalid direction")
+                return -1
+
+            print("[DIRECTION] current direction is now ", current_d)
+            print("[POSITION] Current position is now ", current_x, ",", current_y)
+            
+        elif (move_direction == MOVE_LEFT):
+            if (current_d == UP):
+                current_d = LEFT
+                current_y = current_x - 1
+            elif (current_d == RIGHT):
+                current_d = UP
+                current_x = current_y - 1
+            elif (current_d == DOWN):
+                current_d = RIGHT
+                current_x = current_x + 1
+            elif (current_d == LEFT):
+                current_d = DOWN
+                current_x = current_y + 1
+            else:
+                print("[ERROR] invalid direction")
+                return -1
+
+            print("[DIRECTION] current direction is now ", current_d)
+            print("[POSITION] current position is now ", current_x, ",", current_y)
+                
+        else: # this should never happen, but here it is anyways
+            print("[ERROR] invalid movement direction")
+            return -1
 
     # set speed of the robot, slower is probably more accurate
-    set_speed(200)
+    set_speed(100)
 
-    ## the robot can start from anywhere in the room
+    ## the robot can start from any position in the room
     ## robot will drive forward until it reaches a wall
     ## and then orient itself to the wall
     wall_found = False
+    corrected = False
     while (wall_found != True):
-        dist_left = us_dist(10)
-        dist_front = us_dist(15)
-        if (dist_left > WALL and dist_front > WALL):  # no wall found      
+        # poll sensors and print the value to the console
+        dist_left = us_dist(10) # get distance from left sensor
+        dist_front = us_dist(15) # get distance from front sensor
+        print("[DISTANCE] distance front: ", dist_front)
+        print("[DISTANCE] distance left: ", dist_left)
+        
+        if (dist_left > WALL and dist_front > WALL and corrected == False):  # no wall found      
             # move forward, and stop
             enc_tgt(1,1,MOVE_DIST)
             fwd()
@@ -55,6 +155,16 @@ def map_mode():
 
             # update current position
             updatePos(MOVE_FORWARD)
+
+            # correct if too close to a wall
+            dist_front = us_dist(15)
+            if (dist_front < (WALL - 5):
+                adjust_dist = MOVE_DIST - dist_front
+                enc_tgt(1,1,adjust_dist)
+                fwd()
+                time.sleep(3)
+                stop()
+                corrected = True # if we corrected the distance, we know we are also at a wall
             
         else: # wall found
             wall_found = True
@@ -62,9 +172,9 @@ def map_mode():
             # set current position as the start position
             start_x = current_x
             start_y = current_y
-            print("Start position is now (", start_x, ",", start_y, ")")
+            print ("[START POSITION] start position is ", start_x, ",", start_y)
 
-    if (dist_front == WALL):
+    if (dist_front <= (WALL + 5):
         # turn right
         enc_tgt(0,1,9)
         right_rot()
@@ -87,32 +197,72 @@ def map_mode():
 
     ## now that the start position is different from the current position
     ## we can use the current position and the start position as the stop condition
-    
-    x = 0    
-    while (current_x != start_x and current_y != start_y):
-        x = x + 1
+    ## as the robot moves, it will try to keep itself at a set distance from the wall.
+    while (current_x != start_x or current_y != start_y):
         dist_left = us_dist(10) # get distance from left sensor
-        if (dist_left <= WALL): # wall to the left
-            print("Wall detected, adding to matrix; dist_left = ", dist_left)
+        dist_front = us_dist(15) # get distance from front sensor
+        print("[DISTANCE] distance front: ", dist_front)
+        print("[DISTANCE] distance left: ", dist_left)
+
+        if (dist_left <= (WALL + 25)): # wall to the left
+            print("[EVENT] wall detected, adding to matrix")
 
             # update the matrix
             addWall()
 
-            dist_front = us_dist(15) # get distance from front sensor
             if (dist_front > WALL): # no wall in front
-                print("No obstruction detected, moving forward; dist_front = ", dist_front)
+                print("[EVENT] no obstruction detected, moving forward")
 
+                # flag for movement correction
+                flag = 0
+                
+                # correct position if too close to wall
+                if (dist_left <= (WALL - 5)):
+                    enc_tgt(0,1,2)
+                    right_rot()
+                    time.sleep(1)
+                    flag = 1
+
+                # correct position if too far from wall
+                if (dist_left > WALL + 5):
+                    enc_tgt(1,0,2)
+                    left_rot()
+                    time.sleep(1)
+                    flag = 2                    
+                
                 # move forward, and stop
                 enc_tgt(1,1,MOVE_DIST)
                 fwd()
                 time.sleep(3)
                 stop()
 
+                # if we corrected the direction, correct back
+                if (flag == 1):
+                    enc_tgt(1,0,1)
+                    left_rot()
+                    time.sleep(1)
+                    flag = 0
+
+                if (flag == 2):
+                    enc_tgt(0,1,1)
+                    right_rot()
+                    time.sleep(1)
+                    flag = 0
+
                 # update current position
                 updatePos(MOVE_FORWARD)
 
+                # correct if too close to a wall
+                dist_front = us_dist(15)
+                if (dist_front < (WALL - 5):
+                    adjust_dist = MOVE_DIST - dist_front
+                    enc_tgt(1,1,adjust_dist)
+                    fwd()
+                    time.sleep(3)
+                    stop()
+
             else: # wall in front
-                print("Obstruction detected, making right turn; dist_front = ", dist_front)
+                print("[EVENT] obstruction detected, making right turn")
 
                 # turn right
                 enc_tgt(0,1,9)
@@ -129,7 +279,7 @@ def map_mode():
                 updatePos(MOVE_RIGHT)
 
         else: # no wall to the left
-            print("No wall detected, making left turn; dist_left = ", dist_left)
+            print("[EVENT] no wall detected, making left turn")
 
             # turn left
             enc_tgt(1,0,9)
@@ -146,89 +296,11 @@ def map_mode():
             updatePos(MOVE_LEFT)
 
     ## outside of while loop
-    print("Mapping complete.")
-    print("Printing matrix...")
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-        for row in map_mtx]))
-
-def addWall()
-    # import globals that need to be modified
-    global map_mtx
-
-    if (current_d == UP):
-        map_mtx[current_x - 1][current_y] = X
-        print("(", current_x - 1, ",", current_y, ") added to matrix")
-    elif (current_d == RIGHT):
-        map_mtx[current_x][current_y - 1] = X
-        print("(", current_x, ",", current_y - 1, ") added to matrix")
-    elif (current_d == DOWN):
-        map_mtx[current_x + 1][current_y] = X
-        print("(", current_x + 1, ",", current_y, ") added to matrix")
-    elif (current_d == LEFT):
-        map_mtx[current_x][current_y + 1] = X
-        print("(", current_x, ",", current_y + 1, ") added to matrix")
-    else:
-        print("ERROR: invalid direction")
-
-def updatePos(move_direction)
-    # import globals that need to be modified
-    global current_x
-    global current_y
-    global current_d
-
-    if (move_direction = MOVE_FORWARD):
-        if (current_d == UP):
-            current_y = current_y - 1
-        elif (current_d == RIGHT):
-            current_x = current_x + 1
-        elif (current_d == DOWN):
-            current_y = current_y + 1
-        elif (current_d == LEFT):
-            current_x = current_x - 1
-        else:
-            print("ERROR: invalid direction")
-
-        print("Current position is now (", current_x, ",", current_y, "); direction = ", current_d)
-
-    elif (move_direction = MOVE_RIGHT):
-        if (current_d == UP):
-            current_d = RIGHT
-            current_x = current_x + 1
-        elif (current_d == RIGHT):
-            current_d = DOWN
-            current_x = current_y + 1
-        elif (current_d == DOWN):
-            current_d = LEFT
-            current_y = current_x - 1
-        elif (current_d == LEFT):
-            current_d = UP
-            current_x = current_y - 1
-        else:
-            print("ERROR: invalid direction")
-
-        print("Current position is now (", current_x, ",", current_y, "); direction = ", current_d)
-        
-    elif (move_direction = MOVE_LEFT):
-        if (current_d == UP):
-            current_d = LEFT
-            current_y = current_x - 1
-        elif (current_d == RIGHT):
-            current_d = UP
-            current_x = current_y - 1
-        elif (current_d == DOWN):
-            current_d = RIGHT
-            current_x = current_x + 1
-        elif (current_d == LEFT):
-            current_d = DOWN
-            current_x = current_y + 1
-        else:
-            print("ERROR: invalid direction")
-
-        print("Current position is now (", current_x, ",", current_y, "); direction = ", current_d)
-            
-    else:
-        print("ERROR: invalid movement direction")
-        
+    print("[STOP] mapping complete.")
+    #print("Printing matrix...")
+    #print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+    #    for row in map_mtx]))   
+    return map_mtx     
     
 ## end of definitions
 
